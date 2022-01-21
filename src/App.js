8402c50c273477928fc0e966d2db3905e3f0b2ac
Edon Budakova <ebudakova99@gmail.com>
2022-01-22 00:40:38 +0100

    update

--- src/App.js
+++ src/App.js
@@ -1,25 +1,88 @@
-import logo from './logo.svg';
-import './App.css';
+import React,{useState} from 'react';
+import Password from './components/Password';
 
-function App() {
+const App = () => {
+  const [userInfo, setuserInfo] = useState({
+    password: '',
+  });
+
+  const [isError, setError] = useState(null);
+  const handleChangePassword = (e) => {
+    let password  = e.target.value;
+    setuserInfo({
+      ...userInfo,
+      password:e.target.value
+    });
+    setError(null);
+    let capsCount, smallCount, numberCount, symbolCount
+    //if (password.length < 4) {
+      if (password.length < 6 || (password.match(/(.)\1{2,}/) || []).length) {
+ //   setError("Password must be minimum 4 characters include one UPPERCASE, lowercase, number and special character: @$! % * ? &");
+      setError("Fjalëkalimi duhet të ketë së paku 4 karaktere duke përfshirë një shkronjë të madhe, të vogël, një numër dhe karaktere speciale: @$! % * ? &");
+      return;
+    }
+    else {
+      capsCount = (password.match(/[A-Z]/g) || []).length
+      smallCount = (password.match(/[a-z]/g) || []).length
+      numberCount = (password.match(/[0-9]/g) || []).length
+      symbolCount = (password.match(/\W/g) || []).length
+      if (capsCount < 1) {
+        // setError("Must contain one UPPERCASE letter");
+        setError("Duhet të ketë së paku një shkronjë të madhe");
+        return;
+      }
+      else if (smallCount < 1) {
+        // setError("Must contain one lowercase letter");
+        setError("Duhet të ketë së paku një shkronjë të vogël");
+        return;
+      }
+      else if (numberCount < 1) {
+        // setError("Must contain one number");
+        setError("Duhet të ketë së paku një numër");
+        return;
+      }
+      else if (symbolCount < 1) {
+        // setError("Must contain one special character: @$! % * ? &");
+        setError("Duhet të ketë së paku një karakter special: @$! % * ? &");
+        return;
+      }
+    }
+  }
+  
+  const [isStrength, setStrength] = useState(null);
+  const dataHandler = async (childData) => {
+    setStrength(childData);
+  }
+
+  const onSubmit = async (event) => {
+    try {
+      event.preventDefault();
+      event.persist();
+      console.log(userInfo.password);
+    } catch (error) { throw error;} 
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="box-border m-0 p-14 bg-white">
+    <div className=" max-w-4xl my-0 mx-auto bg-white p-5 shadow-lg rounded-sm text-center ">
+      <h1 className=" bg-white text-center p-5 m-0 text-2xl ">Testimi i nivelit të vështirësisë së fjalëkalimit</h1>
+      <div className="flex justify-between py-5 px-0 flex-col items-center">
+        <form onSubmit={onSubmit} className="flex flex-col items-stretch pt-7 px-0 pb-4 ">
+          <label className=" text-lg mb-3 flex font-semibold " htmlFor="password">
+            Password 
+            {isError !== null && (
+              <p className=" text-red-600 mt-0 mr-0 mb-2 ml-4 max-w-sm text-left "> - {isError}</p>
+            )}
+          </label>
+          <input className=" w-auto h-14 text-xl border-2 border-solid border-white bg-white p-4 rounded-lg shadow-lg " type="password" id="password" name="password" onChange={handleChangePassword} required />
+          <Password password={userInfo.password} actions={dataHandler}/>
+          {isStrength === 'Strong' && 
+            <button type="submit" className=" outline-none h-14 bg-gradient-to-r from-yellow-500 to-red-500 border-0 py-4 px-7 text-white text-lg rounded-lg "  > Fjalëkalimi në rregull </button>
+          }
+        </form>
+      </div>
     </div>
-  );
+    </div>
+  )
 }
-
-export default App;
+export default App
---
+++ src/components/Password.js
@@ -0,0 +1,60 @@
+import React from 'react';
+import './password.css'
+const Password = (props) => {
+
+    const {password} = props;
+
+  const testedResult = password;
+  const createPasswordLabel = () => {
+    let score = 0
+    let regexPositive = ["[A-Z]","[a-z]","[0-9]","\\W",]
+    regexPositive.forEach((regex, index) => {
+      if (new RegExp(regex).test(testedResult)) {
+        score +=1
+      }
+    })
+    switch (score) {
+      case 0:
+        return ({
+          value: 0,
+          info: "",
+        });
+      case 1:
+        return ({
+          value: 1,
+          info: "Weak",
+        });
+      case 2:
+        return ({
+          value: 2,
+          info: "Fair",
+        });
+      case 3:
+        return ({
+          value: 3,
+          info: "Good",
+        });
+      case 4:
+        return ({
+          value: 4,
+          info: "Strong",
+        });
+      default:
+        return null
+    }
+  }
+  {props.actions(createPasswordLabel().info)}
+
+  return (
+    <div className="password-strength-meter">
+      <progress className={` password-strength-meter-progress strength-${createPasswordLabel().info}`} value={createPasswordLabel().value} max="4" />
+      <br />
+      <p className="text-base">
+        {password && ( <>
+          <p className={` password__label strength-${createPasswordLabel().info}`}>Fuqia e fjalëkalimit: <span>{createPasswordLabel().info} </span></p> 
+        </>)}
+      </p>
+    </div>
+   )
+  }
+export default Password;
